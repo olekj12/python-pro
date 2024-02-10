646923a1041360a2300bf6e4357060926086f2f6
olekj12 <159617135+olekj12@users.noreply.github.com>
2024-02-10 10:59:57 +0100

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+meme_dict = {
+            "ez": "skrót od easy, coś łatwego/łatwy przeciwnik do pokonania",
+            "LOL": "Częsta reakcja na coś zabawnego",
+            }
+word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
+if word in meme_dict.keys():
+    # Co powinniśmy zrobić, jeśli słowo zostało znalezione?
+    else: print()
+    # Co powinniśmy zrobić, jeśli słowo nie zostało znalezione?
